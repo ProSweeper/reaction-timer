d802d27314f61199ac37987be85e11d14e8ca337
ProSweeper <dylan.doucet28@gmail.com>
2023-04-07 02:38:23 +0000

    start and finish reaction time game

---
+++ src/components/Results.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2>Results</h2>
+  <p>{{ score }} ms</p>
+  <h3> {{ rank }}</h3>
+</template>
+
+<script>
+  export default {
+    props: ["score",],
+    data() {
+      return {
+        rank: null,
+      }
+    },
+    mounted() {
+      if (this.score < 200) {
+        this.rank = "Sheeeesh"
+      } else if (this.score < 350) {
+        this.rank = "Not tooo bad"
+      } else {
+        this.rank = "Slow Ass"
+      }
+    }
+  }
+</script>
+
+<style>
+  .rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+  }
+</style>
